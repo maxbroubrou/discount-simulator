1c8b06bbad7c46196125ae20bd16f50c021bd642
Maxime Broussart <mbroussart@gmail.com>
2023-10-09 22:45:20 +0200

    feat: update discounts from config JSON files

--- src/classes/store.js
+++ src/classes/store.js
@@ -1,61 +1,53 @@
+import defaultConfig from "../../config/defaultDiscount.json";
+import partnersConfig from "../../config/partnersDiscounts.json";
+
+const NEGATIVE_INFINITE = "-inf";
+const POSITIVE_INFINITE = "inf";
+
 export class Store {
   constructor(discountOffers = []) {
     this.discountOffers = discountOffers;
+    this.partnersRules = this.loadDiscountsRules();
+  }
+  loadDiscountsRules() {
+    let partnersRules = {};
+    for (const partner of partnersConfig) {
+      partnersRules[partner.name] = Object.assign({}, defaultConfig);
+      for (const property of Object.keys(partner)) {
+        if (property !== "name") {
+          // overwrite default discount rules
+          partnersRules[partner.name][property] = partner[property];
+        }
+      }
+    }
+    return partnersRules;
   }
   updateDiscounts() {
-    for (var i = 0; i < this.discountOffers.length; i++) {
-      if (
-        this.discountOffers[i].partnerName != "Naturalia" &&
-        this.discountOffers[i].partnerName != "Vinted"
-      ) {
-        if (this.discountOffers[i].discountInPercent > 0) {
-          if (this.discountOffers[i].partnerName != "Ilek") {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent - 1;
+    for (const offer of this.discountOffers) {
+      const partnerRules =
+        this.partnersRules[offer.partnerName] || defaultConfig;
+      if (partnerRules.canExpires) {
+        offer.expiresIn--;
+        for (const rule of partnerRules.rules) {
+          const { start, end } = rule.expireRange;
+          if (
+            (start === NEGATIVE_INFINITE || offer.expiresIn >= start) &&
+            (end === POSITIVE_INFINITE || offer.expiresIn <= end)
+          ) {
+            offer.discountInPercent += rule.discountChange;
           }
         }
-      } else {
-        if (this.discountOffers[i].discountInPercent < 50) {
-          this.discountOffers[i].discountInPercent =
-            this.discountOffers[i].discountInPercent + 1;
-          if (this.discountOffers[i].partnerName == "Vinted") {
-            if (this.discountOffers[i].expiresIn < 11) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent =
-                  this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-            if (this.discountOffers[i].expiresIn < 6) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent =
-                  this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.discountOffers[i].partnerName != "Ilek") {
-        this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
-      }
-      if (this.discountOffers[i].expiresIn < 0) {
-        if (this.discountOffers[i].partnerName != "Naturalia") {
-          if (this.discountOffers[i].partnerName != "Vinted") {
-            if (this.discountOffers[i].discountInPercent > 0) {
-              if (this.discountOffers[i].partnerName != "Ilek") {
-                this.discountOffers[i].discountInPercent =
-                  this.discountOffers[i].discountInPercent - 1;
-              }
-            }
-          } else {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent -
-              this.discountOffers[i].discountInPercent;
-          }
-        } else {
-          if (this.discountOffers[i].discountInPercent < 50) {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent + 1;
-          }
+
+        // discount limits
+        const isOfferExpired =
+          partnerRules.dropsToZeroWhenExpired && offer.expiresIn < 0;
+        const isDiscountNegative = offer.discountInPercent < 0;
+        if (isOfferExpired || isDiscountNegative) {
+          offer.discountInPercent = 0;
+        } else if (offer.discountInPercent > partnerRules.maxDiscount) {
+          offer.discountInPercent = this.partnersRules[
+            offer.partnerName
+          ].maxDiscount;
         }
       }
     }
