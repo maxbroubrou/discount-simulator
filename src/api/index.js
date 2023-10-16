b28fba11ba1fd660d302f3932dc41632a2fa3dcd
Maxime Broussart <mbroussart@gmail.com>
2023-10-16 09:04:17 +0200

    docs: add API documentation

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import offerController from "./controller/offer.js";
 import simulateController from "./controller/simulate.js";
+import swaggerUi from "swagger-ui-express";
+import jsYaml from "js-yaml";
+import fs from "fs";
 
 const PORT = 3000;
 
@@ -14,6 +17,10 @@ app.delete("/offers/:partnerName", offerController.deleteOffer);
 
 app.get("/simulate/:days", simulateController.simulateDiscount);
 
+const openApiDocument = jsYaml.load(fs.readFileSync("openapi.yaml", "utf-8"));
+
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(openApiDocument, { explorer: true }));
 
 app.listen(PORT, () => {
   /* eslint-disable no-console */
